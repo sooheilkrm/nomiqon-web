bc3e25679ade3ab5268b60d63abb5d85f85d5289
sooheilkrm <sooheil75@yahoo.com>
2025-06-20 19:56:42 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-gray-800">Welcome to Nomiqon</h1>
+    <div className="min-h-screen flex items-center justify-center bg-white text-black">
+      <h1 className="text-4xl font-bold">Welcome to Nomiqon</h1>
     </div>
   );
 }
