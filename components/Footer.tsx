b54d8bb5e8bc4372c3295557f6a708407ed5b12a
sooheilkrm <sooheil75@yahoo.com>
2025-06-20 19:23:58 +0200

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-4 bg-gray-100 text-center">
-      <p className="text-sm text-gray-500">© 2025 Nomiqon. All rights reserved.</p>
-    </footer>
-  )
-}
--- components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="p-4 bg-white shadow">
-      <div className="container mx-auto text-xl font-bold">Nomiqon</div>
-    </header>
-  )
-}
