c819cd405e974b2f32ba96b8df468f6f699be2b9
sooheilkrm <sooheil75@yahoo.com>
2025-06-21 00:59:55 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,1 @@
-module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        brand: {
-          DEFAULT: '#14b8a6',
-          light: '#2dd4bf',
-          dark: '#0f766e',
-        },
-      },
-    },
-  },
-  plugins: [],
-}
+module.exports = { content: ['./pages/**/*.{js,jsx}'], theme: { extend: {} }, plugins: [], }
