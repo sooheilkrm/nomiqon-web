6b4dd32b5836108bd7a72b82a4e5b53e329937dd
sooheilkrm <sooheil75@yahoo.com>
2025-06-20 19:44:43 +0200

    Delete components directory

--- components/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ label, onClick }) {
-  return (
-    <button onClick={onClick} className="px-4 py-2 bg-blue-600 text-white rounded">
-      {label}
-    </button>
-  );
-}
