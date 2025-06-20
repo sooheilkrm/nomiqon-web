6e245877757f541f088b5253f48c663bd8c59ae3
sooheilkrm <sooheil75@yahoo.com>
2025-06-20 19:16:20 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to Nomiqon 🚀</h1>
-    </main>
-  );
-}
