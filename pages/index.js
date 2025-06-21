cb5fb980674ac2515671dddced1b8c2467068073
sooheilkrm <sooheil75@yahoo.com>
2025-06-21 03:48:11 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,21 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import Features from '../components/Features'
-import Footer from '../components/Footer'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Features from '../components/Features';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold text-center mt-20 text-teal-600">
-      Welcome to Nomiqon
-    </div>
-  )
-}
     <>
       <Head>
         <title>Nomiqon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Navbar />
       <Hero />
       <Features />
       <Footer />
     </>
-  )
+  );
 }
