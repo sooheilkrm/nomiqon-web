f3da39742fe8e2c603da43630fd7e606475b860a
sooheilkrm <sooheil75@yahoo.com>
2025-06-21 15:25:15 +0200

    Add files via upload

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,1 @@
-export default function Footer() {
-  return (
-    <footer className="bg-white border-t border-gray-200 py-6 text-center text-sm text-gray-500">
-      <p>© 2025 Nomiqon. All rights reserved.</p>
-      <div className="mt-2 space-x-4">
-        <a href="#" className="hover:underline">Instagram</a>
-        <a href="#" className="hover:underline">Twitter</a>
-        <a href="#" className="hover:underline">Facebook</a>
-      </div>
-    </footer>
-  );
-}
+// Long footer similar to Looka
