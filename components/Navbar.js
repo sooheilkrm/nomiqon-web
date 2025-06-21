5808da30dd05cbd0cb1814ba69f48c21cf4bf6df
sooheilkrm <sooheil75@yahoo.com>
2025-06-21 14:38:31 +0200

    Add files via upload

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,14 @@
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
     <nav className="bg-white border-b border-gray-200 px-4 py-3 flex justify-between items-center">
       <h1 className="text-xl font-bold text-gray-900">Nomiqon</h1>
       <div className="space-x-4">
-        <a href="#features" className="text-gray-600 hover:text-black">Features</a>
-        <a href="#about" className="text-gray-600 hover:text-black">About</a>
-        <a href="#contact" className="text-gray-600 hover:text-black">Contact</a>
+        <Link href="/about" className="text-gray-600 hover:text-black">About</Link>
+        <Link href="/contact" className="text-gray-600 hover:text-black">Contact</Link>
+        <Link href="/blog" className="text-gray-600 hover:text-black">Blog</Link>
+        <Link href="/brand-form" className="text-gray-600 hover:text-black">Create Brand</Link>
       </div>
     </nav>
   );
