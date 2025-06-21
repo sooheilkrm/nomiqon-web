830709c69f429a87c7848fff475c098871a4285b
sooheilkrm <sooheil75@yahoo.com>
2025-06-21 14:44:31 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
     <nav className="bg-white border-b border-gray-200 px-4 py-3 flex justify-between items-center">
       <h1 className="text-xl font-bold text-gray-900">Nomiqon</h1>
       <div className="space-x-4">
-        <Link href="/about" className="text-gray-600 hover:text-black">About</Link>
-        <Link href="/contact" className="text-gray-600 hover:text-black">Contact</Link>
-        <Link href="/blog" className="text-gray-600 hover:text-black">Blog</Link>
-        <Link href="/brand-form" className="text-gray-600 hover:text-black">Create Brand</Link>
+        <Link href="/about" legacyBehavior><a className="text-gray-600 hover:text-black">About</a></Link>
+        <Link href="/contact" legacyBehavior><a className="text-gray-600 hover:text-black">Contact</a></Link>
+        <Link href="/blog" legacyBehavior><a className="text-gray-600 hover:text-black">Blog</a></Link>
+        <Link href="/brand-form" legacyBehavior><a className="text-gray-600 hover:text-black">Create Brand</a></Link>
       </div>
     </nav>
   );
